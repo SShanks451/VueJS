563c18f28bd746ffddffc6aa6b2510706a053583
Shashank Suman <103516291+SShanks451@users.noreply.github.com>
2024-10-22 15:17:27 +0530

    commit

---
+++ Section-8/cmp-communication-01-starting-setup/src/components/FriendContact.vue
@@ -0,0 +1,73 @@
+<template>
+  <li>
+    <h2>{{ name }} {{ isFavourite ? "(Favourite)" : "" }}</h2>
+    <button @click="toggleFavourite">Toggle favourite</button>
+    <button @click="toggleDetails">{{ detailsAreVisible ? "Hide" : "Show" }} Details</button>
+    <ul v-if="detailsAreVisible">
+      <li>
+        <strong>Phone:</strong>
+        {{ phoneNumber }}
+      </li>
+      <li>
+        <strong>Email:</strong>
+        {{ emailAddress }}
+      </li>
+    </ul>
+    <button @click="$emit('delete', id)">Delete</button>
+  </li>
+</template>
+
+<script>
+export default {
+  //  props: ["name", "phoneNumber", "emailAddress", "isFavourite"],
+  props: {
+    id: {
+      type: String,
+      required: String,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    phoneNumber: {
+      type: String,
+      required: true,
+    },
+    emailAddress: {
+      type: String,
+      required: true,
+    },
+    isFavourite: {
+      type: Boolean,
+      required: false,
+      default: false,
+      // validator: function (value) {
+      //   return value === "1" || value === "0";
+      // },
+    },
+  },
+  emits: ["toggle-favourite", "delete"],
+  // emits: {
+  //   "toggle-favourite": function (id) {
+  //     if (id) {
+  //       return true;
+  //     } else {
+  //       console.warn("Id is missing!");
+  //     }
+  //   },
+  // },
+  data() {
+    return {
+      detailsAreVisible: false,
+    };
+  },
+  methods: {
+    toggleDetails() {
+      this.detailsAreVisible = !this.detailsAreVisible;
+    },
+    toggleFavourite() {
+      this.$emit("toggle-favourite", this.id);
+    },
+  },
+};
+</script>
